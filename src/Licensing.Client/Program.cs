bd82b57f6964fb08f7c09da02d1e4627f67c0a6f
SATRIAMILAN <satriansyahw@gmail.com>
2026-04-25 13:01:06 +0700

    test: finalize robust Playwright E2E test covering full lifecycle

--- src/Licensing.Client/Program.cs
+++ src/Licensing.Client/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<PersonaService>();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
